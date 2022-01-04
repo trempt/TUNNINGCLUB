c3dd52d9e262d3299c13e150839e8fbd6e6ddaf4
trempt <95629562+trempt@users.noreply.github.com>
2022-01-04 22:06:23 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/`, // Change to `/running_page` when running on github pages
+  pathPrefix: `TUNNINGCLUB`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'TED Running Club',
     siteUrl: 'https://tunningclub.vercel.app',
